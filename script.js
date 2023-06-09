ad98c7a2137f01f5c1e161e7465666e5a3c2d13b
Allen Mathews <allenjoemathews@gmail.com>
2023-06-09 17:38:36 +1000

    added functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+let guest = 0;
+let home = 0;
+document.getElementById('guest').textContent = guest;
+document.getElementById('home').textContent = home;
+
+let score = document.getElementById('score')
+
+function plusone() {
+    score = guest + 1;
+    score.textContent = "Score: " + score;
+}
